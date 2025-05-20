da5d06a2dcaaef6c8d9d74f9debfaa62c3aeaa52
sahdkhan <03sahd@gmail.com>
2025-05-19 22:02:46 -0700

    Updated picture and fixed sizing of hero arrow

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,58 +1,62 @@
 import React from 'react';
 import img1 from '../assets/selfie1.jpeg';
-import cv from '../assets/Sahd_Khan_Official_Resume.pdf';
+import img2 from '../assets/selfie3.jpg';
+import img3 from '../assets/selfie4.jpg';
+import cv from '../assets/Sahd_Khan_Software_Developer.pdf';
 import { motion } from 'framer-motion';
 
 export default function AboutMe() {
-  return (
-    <div id="about" className="w-full flex-col flex lg:items-center lg:flex-row justify-center  ">
-      {/* Col1 container */}
-      <div className="flex flex-col h-2/3 lg:w-1/2 items-center">
-        {/* Col1 Images */}
-        <img
-          src={img1}
-          alt="Sahd Khan"
-          className="max-lg:size-80 lg:size-96 rounded-lg relative mt-10 lg:mt-0"
-        />
-      </div>
-      {/* Col2; text */}
-      <motion.div
-        className="lg:w-1/2 p-10 max-sm:p-5 "
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 2 }}>
-        <p className="lg:text-4xl md:text-2xl text-xl font-bold mb-5 font-main tracking-wider text-black ">
-          Hey there! I'm Sahd
-        </p>
-        <p className="text-xs md:text-sm lg:text-base  mb-5 font-main tracking-wide text-gray-500 text-justify xl:w-full">
-          I am a new grad from California State University Sacramento with a B.S. in Computer
-          Science. I have a passion for building software and learning new technologies. I am
-          currently looking for a full-time position as a software engineer/Web Developer but am
-          open to working wherever I feel wanted and respected. I am a very hard worker and am
-          always looking to improve my skills. I take great pride in my problem-solving mentality
-          and my ability to work well with others. I feel my strongest characteristic is my ability
-          to learn quickly and adapt to new situations.
-        </p>
-        <p className="text-xs md:text-sm lg:text-base  mb-5 font-main tracking-wide text-gray-500 text-justify xl:w-full">
-          I've learned a lot in my time at CSUS and have had the opportunity to work on some really
-          great projects. Many of these projects involved working in a team environment and
-          collaborating with others to achieve a common goal which is something I really enjoy. I've
-          also had the opportunity to work on some projects on my own which has helped me develop my
-          problem-solving skills and my ability to work independently. I've also had the opportunity
-          to work on some projects that have been deployed to the web and mobile devices. I've
-          learned a lot about the software development lifecycle and have gained a lot of experience
-          working with different technologies.
-        </p>
-        {/* Button to download CV */}
-        <a href={cv} download="Sahd_Khan_Resume" target="_blank" rel="noreferrer">
-          <motion.button
-            className="bg-[#112A46] text-white font-bold py-2 px-4 rounded-full"
-            whileTap={{ scale: 0.85 }}
-            whileHover={{ scale: 1.1, backgroundColor: '#245893' }}>
-            Download CV
-          </motion.button>
-        </a>
-      </motion.div>
-    </div>
-  );
+	return (
+		<div id="about" className="w-full flex-col flex lg:items-center lg:flex-row justify-center  ">
+			{/* Col1 container */}
+			<div className="flex flex-col h-2/3 lg:w-1/2 items-center">
+				{/* Col1 Images */}
+				<img
+					src={img3}
+					alt="Sahd Khan"
+					className="max-lg:size-80 lg:size-96 rounded-lg relative mt-10 lg:mt-0"
+				/>
+			</div>
+			{/* Col2; text */}
+			<motion.div
+				className="lg:w-1/2 p-10 max-sm:p-5 "
+				initial={{ opacity: 0 }}
+				whileInView={{ opacity: 1 }}
+				transition={{ duration: 2 }}
+			>
+				<p className="lg:text-4xl md:text-2xl text-xl font-bold mb-5 font-main tracking-wider text-black ">
+					Hey there! I'm Sahd
+				</p>
+				<p className="text-xs md:text-sm lg:text-base  mb-5 font-main tracking-wide text-gray-500 text-justify xl:w-5/6">
+					I am a new grad from California State University Sacramento with a B.S. in Computer
+					Science. I have a passion for building software and learning new technologies. I am
+					currently looking for a full-time position as a software engineer/Web Developer but am
+					open to working wherever I feel wanted and respected. I am a very hard worker and am
+					always looking to improve my skills. I take great pride in my problem-solving mentality
+					and my ability to work well with others. I feel my strongest characteristic is my ability
+					to learn quickly and adapt to new situations.
+				</p>
+				<p className="text-xs md:text-sm lg:text-base  mb-5 font-main tracking-wide text-gray-500 text-justify xl:w-5/6">
+					I've learned a lot in my time at CSUS and have had the opportunity to work on some really
+					great projects. Many of these projects involved working in a team environment and
+					collaborating with others to achieve a common goal which is something I really enjoy. I've
+					also had the opportunity to work on some projects on my own which has helped me develop my
+					problem-solving skills and my ability to work independently. I've also had the opportunity
+					to work on some projects that have been deployed to the web and mobile devices. I've
+					learned a lot about the software development lifecycle and have gained a lot of experience
+					working with different technologies.
+				</p>
+				{/* Button to download CV */}
+				<a href={cv} download="Sahd_Khan_Resume" target="_blank" rel="noreferrer">
+					<motion.button
+						className="bg-[#112A46] text-white font-bold py-2 px-4 rounded-full"
+						whileTap={{ scale: 0.85 }}
+						whileHover={{ scale: 1.1, backgroundColor: '#245893' }}
+					>
+						Download CV
+					</motion.button>
+				</a>
+			</motion.div>
+		</div>
+	);
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,46 +4,47 @@ import { Link } from 'react-scroll';
 import { TypeAnimation } from 'react-type-animation';
 
 export default function Hero() {
-  const professions = [
-    'Software Engineer',
-    'Mobile App Developer',
-    'Web Developer',
-    'Full Stack Developer',
-    'Backend Developer',
-  ];
+	const professions = [
+		'Software Engineer',
+		'Mobile App Developer',
+		'Web Developer',
+		'Full Stack Developer',
+		'Backend Developer',
+	];
 
-  return (
-    <div
-      id="top"
-      className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-r from-sky-100 via-blue-100 via-50% to-[#ffff] to-50%">
-      <div className="flex-col w-1/2 h-1/2">
-        <h1 className="text-xl font-bold text-center uppercase mb-5 font-main tracking-wider text-[#A4CFFB] drop-shadow-sm">
-          Hey! I am
-        </h1>
-        <h1 className="text-7xl font-bold text-center mb-5 font-main text-[#112A46] drop-shadow-sm">
-          Sahd Khan
-        </h1>
-        <h1 className="text-4xl font-bold text-center mb-5 font-main tracking-wider text-[#112A46] drop-shadow-sm">
-          I'm a{' '}
-          <span className="text-[#A4CFFB] font-main drop-shadow-sm">
-            {/* <ReactTyped strings={professions} typeSpeed={40} backSpeed={50} loop /> */}
-            <TypeAnimation
-              cursor={false}
-              sequence={professions}
-              wrapper="span"
-              repeat={Infinity}
-              speed={10}
-              repeatType="loop"
-            />
-          </span>
-        </h1>
-      </div>
-      <div className="bg-blue-100 w-16 h-16 absolute bottom-32 max-md:bottom-24 rotate-45"></div>
-      <div className="absolute bottom-36 max-md:bottom-28">
-        <Link to="projects" spy={true} smooth={true}>
-          <FaArrowDownLong size={24} className=" text-[#A4CFFB] animate-bounce cursor-pointer" />
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			id="top"
+			className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-r from-sky-100 via-blue-100 via-50% to-[#ffff] to-50%"
+		>
+			<div className="flex-col w-1/2 h-1/2">
+				<h1 className="text-xl font-bold text-center uppercase mb-5 font-main tracking-wider text-[#A4CFFB] drop-shadow-sm">
+					Hey! I am
+				</h1>
+				<h1 className="text-7xl font-bold text-center mb-5 font-main text-[#112A46] drop-shadow-sm">
+					Sahd Khan
+				</h1>
+				<h1 className="text-4xl font-bold text-center mb-5 font-main tracking-wider text-[#112A46] drop-shadow-sm">
+					I'm a{' '}
+					<span className="text-[#A4CFFB] font-main drop-shadow-sm">
+						{/* <ReactTyped strings={professions} typeSpeed={40} backSpeed={50} loop /> */}
+						<TypeAnimation
+							cursor={false}
+							sequence={professions}
+							wrapper="span"
+							repeat={Infinity}
+							speed={10}
+							repeatType="loop"
+						/>
+					</span>
+				</h1>
+			</div>
+			<div className="bg-blue-100 w-16 h-16 absolute bottom-32 max-md:bottom-24 max-sm:bottom-6 rotate-45"></div>
+			<div className="absolute bottom-36 max-md:bottom-28 max-sm:bottom-10">
+				<Link to="projects" spy={true} smooth={true}>
+					<FaArrowDownLong size={24} className=" text-[#A4CFFB] animate-bounce cursor-pointer" />
+				</Link>
+			</div>
+		</div>
+	);
 }
